24b9b945667c160d9ce996544d90762ca369b4d3
AsfarHorani <asfarh281@gmail.com>
2022-09-14 00:43:28 +0500

    first commit

---
+++ app/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace app
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+        }
+    }
+
+
+    class Person{
+       public string name;
+       public string age; 
+       public string relation;
+       private long phoneNo; 
+
+
+    //    public long getPhoneNo(){
+    //         // Console.WriteLine("What is yout age?");
+    //         // string age = Console.ReadLine();
+    //         // if(age==this.age)
+    //         //     return phoneNo;
+
+    //         return phoneNo;    
+    //      }
+
+    //     public void setPhoneNo(long n){
+    //         this.phoneNo = n;
+    //     }
+
+    public string PhoneNo   // property
+  {
+    get {return phoneNo; }   // get method
+    set { phoneNo = value; }  // set method
+  }
+
+
+        public Person(string _name, int _age, string _relation){
+                this.name = _name;
+                this.age = _age;
+                this.relation = _relation;
+        }
+
+        public string toString(){
+            return name+","+age+","+relation;
+        }
+    }
+}
